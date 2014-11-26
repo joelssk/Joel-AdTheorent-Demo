f00d563c8eda578cc5d73306b770972bd4a5893e
Joel McCauley <joelssk@live.com>
2014-11-26 13:26:44 -0500

    Adtheorent Demo
    Project index

---
+++ inc/config.php
@@ -0,0 +1,11 @@
+<?php
+define("HOST", "mysql17.000webhost.com");
+define("USER", "a3036521_adt"); 
+define("PASSWORD", "omgomg123");
+define("DATABASE", "a3036521_adt");
+ 
+define("CAN_REGISTER", "any");
+define("DEFAULT_ROLE", "member");
+ 
+define("SECURE", FALSE);
+?>
